def01cc2bda384aed3eb9a87dffd4fa327b7892a
DEBASIS SAHA <saha.debasis@gmail.com>
2021-07-12 22:40:22 +0530

    Day3 code uploaded

---
+++ Day_03/02_Razor_Page_End/MVC_Sample_Application/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MVC_Sample_Application.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_Sample_Application.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {
+            
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string WelcomeMessage()
+        {
+            return "Hello to Asp.Net Core MVC Framework...";
+        }
+
+    }
+}
