80409b46a0cacb8ce765323387b37e54c5d4d3d1
DEBASIS SAHA <saha.debasis@gmail.com>
2021-07-18 20:22:49 +0530

    Day-05 Code Upload

---
+++ Day-05/1_action_param_start/MVC_Sample_Application/Controllers/Day4Controller.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Sample_Application.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_Sample_Application.Controllers
+{
+    public class Day4Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult HtmlHelper()
+        {
+            return View();
+        }
+
+        public IActionResult TagHelperDemo()
+        {
+            IList<Product> products = new List<Product>{
+                new Product() { ProductName = "Samsung Galaxy 10", Category="Mobile", Price = 65000, DiscountRate=7.5 } ,
+                new Product() { ProductName = "Samsung Note 20", Category="Mobile", Price = 105000, DiscountRate=8.5 } ,
+                new Product() { ProductName = "Canon EOS M50 Mark II", Category="Camera", Price = 58990, DiscountRate=11.25 } ,
+                new Product() { ProductName = "HP Envy 11th Gen Intel Core i7", Category="Laptop", Price = 11500, DiscountRate=9.25 } ,
+                new Product() { ProductName = "Redmi 10T", Category="Mobile", Price = 26500, DiscountRate=7.5 }
+            };
+            return View(products);
+        }
+    }
+}
---
+++ Day-05/2_partial_view_end/MVC_Sample_Application/Controllers/BlogController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_Sample_Application.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Index()
+        {
+            return new ContentResult { Content = "Blog Related Information Details" };
+        }
+
+        public IActionResult Posts(string id)
+        {
+            return new ContentResult { Content = string.Format("Blog Post Title : {0}", id) };
+        }
+
+        public IActionResult PostCount(int id)
+        {
+            return new ContentResult { Content = string.Format("Blog Post Count : {0}", id.ToString()) };
+        }
+
+        [Route("blog/post/{year:int}/{month:int}/{title}")]
+        public IActionResult Post(int year, int month, string title)
+        {
+            return new ContentResult { Content = string.Format("Blog Post Details : Year {0} Month: {1} Title: {2}", year, month, title) };
+        }
+    }
+}
